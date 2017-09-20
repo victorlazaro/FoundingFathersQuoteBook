7e3657f553eac4e4d1d5ff6249815ec251580dba
victorlazaro <12ohcutiv>
2017-09-19 22:40:51 -0600

    Initial Commit

---
+++ Founding Fathers Quote Book 2Tests/Founding_Fathers_Quote_Book_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Founding_Fathers_Quote_Book_2Tests.swift
+//  Founding Fathers Quote Book 2Tests
+//
+//  Created by Victor Lazaro on 9/19/17.
+//  Copyright © 2017 Victor Lazaro. All rights reserved.
+//
+
+import XCTest
+@testable import Founding_Fathers_Quote_Book_2
+
+class Founding_Fathers_Quote_Book_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
